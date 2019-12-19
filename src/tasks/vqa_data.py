343efb884347c93b32935f7ecdd5c1260673b205
Corentin Dancette <corentin.dancette@gmail.com>
2019-12-19 11:42:47 +0100

    add nms

--- src/tasks/vqa_data.py
+++ src/tasks/vqa_data.py
@@ -83,6 +83,7 @@ class VQATorchDataset(Dataset):
     def __init__(self, dataset: VQADataset):
         super().__init__()
         self.raw_dataset = dataset
+        self.nms = args.nms
 
         if args.tiny:
             topk = TINY_IMG_NUM
@@ -117,6 +118,32 @@ class VQATorchDataset(Dataset):
     def __len__(self):
         return len(self.data)
 
+def bb_intersection_over_union(self, boxA, boxB):
+    """
+    from https://www.pyimagesearch.com/2016/11/07/intersection-over-union-iou-for-object-detection/
+    """
+	# determine the (x, y)-coordinates of the intersection rectangle
+	xA = max(boxA[0], boxB[0])
+	yA = max(boxA[1], boxB[1])
+	xB = min(boxA[2], boxB[2])
+	yB = min(boxA[3], boxB[3])
+ 
+	# compute the area of intersection rectangle
+	interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+ 
+	# compute the area of both the prediction and ground-truth
+	# rectangles
+	boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+	boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+ 
+	# compute the intersection over union by taking the intersection
+	# area and dividing it by the sum of prediction + ground-truth
+	# areas - the interesection area
+	iou = interArea / float(boxAArea + boxBArea - interArea)
+ 
+	# return the intersection over union value
+	return iou
+
     def __getitem__(self, item: int):
         datum = self.data[item]
 
@@ -129,6 +156,22 @@ class VQATorchDataset(Dataset):
         obj_num = img_info['num_boxes']
         feats = img_info['features'].copy()
         boxes = img_info['boxes'].copy()
+
+        
+        if self.nms is not None:
+            discard = {}
+            for i, bboxA in enumerate(bboxes):
+                for j, boxB in enumerate(boxes):
+                    iou = self.bb_intersection_over_union(boxA, boxB)
+                    if iou >= self.nms:
+                        # eliminate the bbox with less confidence (they are sorted by confidence)
+                        discard.add(j) 
+
+            keep = [i for i in range(len(boxes)) if i not in discard]
+            obj_num = len(keep)
+            feats = feats[keep]
+            boxes = boxes[keep]
+
         assert obj_num == len(boxes) == len(feats)
 
         # Normalize the boxes (to 0 ~ 1)
