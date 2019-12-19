066c6df2fe0af94b33f981b65a9d883e6c551ce0
Corentin Dancette <corentin.dancette@gmail.com>
2019-12-19 11:50:10 +0100

    fix

--- src/tasks/vqa_data.py
+++ src/tasks/vqa_data.py
@@ -127,20 +127,20 @@ def bb_intersection_over_union(self, boxA, boxB):
     yA = max(boxA[1], boxB[1])
     xB = min(boxA[2], boxB[2])
     yB = min(boxA[3], boxB[3])
- 
+
     # compute the area of intersection rectangle
     interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
- 
+
     # compute the area of both the prediction and ground-truth
     # rectangles
     boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
     boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
- 
+
     # compute the intersection over union by taking the intersection
     # area and dividing it by the sum of prediction + ground-truth
     # areas - the interesection area
     iou = interArea / float(boxAArea + boxBArea - interArea)
- 
+
     # return the intersection over union value
     return iou
 
@@ -157,7 +157,7 @@ def bb_intersection_over_union(self, boxA, boxB):
         feats = img_info['features'].copy()
         boxes = img_info['boxes'].copy()
 
-        
+
         if self.nms is not None:
             discard = {}
             for i, bboxA in enumerate(bboxes):
@@ -165,7 +165,7 @@ def bb_intersection_over_union(self, boxA, boxB):
                     iou = self.bb_intersection_over_union(boxA, boxB)
                     if iou >= self.nms:
                         # eliminate the bbox with less confidence (they are sorted by confidence)
-                        discard.add(j) 
+                        discard.add(j)
 
             keep = [i for i in range(len(boxes)) if i not in discard]
             obj_num = len(keep)
